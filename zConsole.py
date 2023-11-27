7ef57325b6a37f964a77f55ed9738286b6ea2ea0
Aadi Kenchammana <aadikenchammana@gmail.com>
2023-11-26 21:04:23 -0800

    s

--- zConsole.py
+++ zConsole.py
@@ -7,9 +7,9 @@ from zPDfunction import PD
 if __name__ == "__main__":
     # create two processes
     print("zCONSOLE: CORRECT VERSION")
-    p1 = multiprocessing.Process(target=DC)
+    #p1 = multiprocessing.Process(target=DC)
     p2 = multiprocessing.Process(target=OD)
-    p3 = multiprocessing.Process(target=PD)
+    #p3 = multiprocessing.Process(target=PD)
 
 
     with open('zODworkspace//watchlist.txt', 'w') as f:
@@ -17,13 +17,13 @@ if __name__ == "__main__":
     with open('zODworkspace//total_watchlist.txt', 'w') as f:
         json.dump({}, f)
 
-    p1.start()
+    #p1.start()
     time.sleep(1)
     p2.start()
     time.sleep(0.5)
-    p3.start()
+    #p3.start()
 
     # wait for the processes to finish
-    p1.join()
+    #p1.join()
     p2.join()
-    p3.join()
+    #p3.join()
