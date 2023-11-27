c9347d0447528f9e04536c12a17de6f6263377a7
Aadi Kenchammana <aadikenchammana@gmail.com>
2023-11-26 21:07:50 -0800

    s

--- zConsole.py
+++ zConsole.py
@@ -4,26 +4,26 @@ import json
 from zDCfunction import DC
 from zODfunction import OD
 from zPDfunction import PD
-if __name__ == "__main__":
-    # create two processes
-    print("zCONSOLE: CORRECT VERSION")
-    #p1 = multiprocessing.Process(target=DC)
-    p2 = multiprocessing.Process(target=OD)
-    #p3 = multiprocessing.Process(target=PD)
+#if __name__ == "__main__":
+# create two processes
+print("zCONSOLE: CORRECT VERSION")
+#p1 = multiprocessing.Process(target=DC)
+p2 = multiprocessing.Process(target=OD)
+#p3 = multiprocessing.Process(target=PD)
 
 
-    with open('zODworkspace//watchlist.txt', 'w') as f:
-        json.dump({}, f)
-    with open('zODworkspace//total_watchlist.txt', 'w') as f:
-        json.dump({}, f)
+with open('zODworkspace//watchlist.txt', 'w') as f:
+    json.dump({}, f)
+with open('zODworkspace//total_watchlist.txt', 'w') as f:
+    json.dump({}, f)
 
-    #p1.start()
-    time.sleep(1)
-    p2.start()
-    time.sleep(0.5)
-    #p3.start()
+#p1.start()
+time.sleep(1)
+p2.start()
+time.sleep(0.5)
+#p3.start()
 
-    # wait for the processes to finish
-    #p1.join()
-    p2.join()
-    #p3.join()
+# wait for the processes to finish
+#p1.join()
+p2.join()
+#p3.join()
